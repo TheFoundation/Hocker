cde1cd2f581268a6573961ef64e9fcb45c0dd230
benchonaut <benchonaut@yandex.com>
2018-06-16 16:47:22 +0200

    2018-06-16-14-47

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -18,6 +18,11 @@ fi
 chown root:root ${SSH_KEY_DSS}
 chmod 600 ${SSH_KEY_DSS}
 
+if [ "$INSTALL_MONGOB" = "true" ] ; then 
+	test -d /etc/mongodb || mkdir /etc/mongodb
+	test -f /etc/mongodb/mongodb.conf || (mv /etc/mongodb.conf /etc/mongodb/mongodb.conf ; ln -s /etc/mongodb/mongodb.conf /etc/mongodb.conf )
+fi
+
 if [ ! -f ${SSH_KEY_RSA} ]; then
     dropbearkey  -t rsa -f ${SSH_KEY_RSA} -s 2048
 fi
@@ -76,9 +81,27 @@ if [ "${INSTALL_MARIADB}" = "true" ]; then
 		#mysql --batch --silent -uroot -e "use mysql;update user set authentication_string=password('"${MARIADB_ROOT_PASSWORD}"') where user='root'; flush privileges;" || echo "seems like MARIADB_ROOT_PASSWORD was already set" 
 		sed -i 's/^password.\+/password = '$MARIADB_ROOT_PASSWORD'/g' /etc/mysql/debian.cnf ; kill $(pidof mysqld);sleep 3 ;/etc/init.d/mysql start ) & 
 	fi 
+	if [ "${MARIADB_DATABASE}" = "true" ]; then
+			    SQL1="CREATE DATABASE IF NOT EXISTS ${MARIADB_USERNAME};"
+				SQL2="CREATE USER '${MARIADB_USERNAME}'@'%' IDENTIFIED BY '${MARIADBDB_PASSWORD}';"
+				SQL3="GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USERNAME}'@'%';"
+			    SQL4="FLUSH PRIVILEGES;"
+			
+			    if [ -f /root/.my.cnf ]; then
+			        $BIN_MYSQL -e "${SQL1}${SQL2}${SQL3}${SQL4}"
+			    else
+			        # If /root/.my.cnf doesn't exist then it'll take .env setting
+			        $BIN_MYSQL -h $DB_HOST -u root -p${MARIADB_ROOT_PASSWORD} -e "${SQL1}${SQL2}${SQL3}${SQL4}"
+    				fi
+	else
+		echo "MYSQL DB NOT IN .env"
+	fi
+	
 else
    echo MARIADB not marked for installation ,
 fi
+test -f /etc/apache2/sites-available/default-ssl.conf || cp /etc/apache2/sites-available.default/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf 
+test -f /etc/apache2/sites-available/000-default.conf || cp /etc/apache2/sites-available.default/000-default.conf /etc/apache2/sites-available/000-default.conf 
 
 exec a2ensite 000-default &
 exec a2ensite default-ssl &
