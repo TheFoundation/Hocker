e92bf5531eb5a121789ec3eabd2063ebd72e25ab
benchonaut <benchonaut@yandex.com>
2018-08-21 12:39:42 +0200

    2018-08-21-10-39

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -120,8 +120,8 @@ fi
 test -f /etc/apache2/sites-available/default-ssl.conf || cp /etc/apache2/sites-available.default/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf 
 test -f /etc/apache2/sites-available/000-default.conf || cp /etc/apache2/sites-available.default/000-default.conf /etc/apache2/sites-available/000-default.conf 
 
-grep  "php_admin_value error_log" /etc/apache2/sites-available/000-default.conf || sed 's/AllowOverride All/AllowOverride All\nphp_admin_value error_log ${APACHE_LOG_DIR}\/php.error.log/g' /etc/apache2/sites-available/000-default.conf
-grep  "php_admin_value error_log" /etc/apache2/sites-available/default-ssl.conf ||  sed 's/AllowOverride All/AllowOverride All\nphp_admin_value error_log ${APACHE_LOG_DIR}\/php.error.log/g' /etc/apache2/sites-available/default-ssl.conf
+grep  "php_admin_value error_log" /etc/apache2/sites-available/000-default.conf || sed -i 's/AllowOverride All/AllowOverride All\nphp_admin_value error_log ${APACHE_LOG_DIR}\/php.error.log/g' /etc/apache2/sites-available/000-default.conf
+grep  "php_admin_value error_log" /etc/apache2/sites-available/default-ssl.conf || sed -i 's/AllowOverride All/AllowOverride All\nphp_admin_value error_log ${APACHE_LOG_DIR}\/php.error.log/g' /etc/apache2/sites-available/default-ssl.conf
 
 exec a2ensite 000-default &
 exec a2ensite default-ssl &
