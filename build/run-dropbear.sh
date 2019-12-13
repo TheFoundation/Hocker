df11ad4c7922016318649622523defe7c60a0f5c
Benjamin Freitag <benchonaut@yandex.com>
2019-12-13 15:03:45 +0100

    2019-12-13-14.03

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -43,6 +43,7 @@ test -d /var/www/html || ( mkdir /var/www/html;chown www-data:www-data /var/www/
 
 ##fixing legacy composer version from ubuntu
 (
+cd /tmp/
 	EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 	ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
@@ -63,9 +64,9 @@ test -d /var/www/html || ( mkdir /var/www/html;chown www-data:www-data /var/www/
 	newest=$( (./composer.phar --version 2>/dev/null;composer --version 2>/dev/null)|sed 's/Composer version/Composer/g'|cut -d" " -f2-3|sed 's/ /-/g'|sort -n |tail -n1)
 	upgrade=$(./composer.phar --version 2>/dev/null |sed 's/Composer version/Composer/g'|cut -d" " -f2-3|sed 's/ /-/g' )
 	sysver=$(composer --version 2>/dev/null |sed 's/Composer version/Composer/g'|cut -d" " -f2-3|sed 's/ /-/g' )
-	if [ "$sysver" != "$newest" ] ;then echo UPGRADING COMPOSER ;fi 
+	if [ "$sysver" != "$newest" ] ;then echo UPGRADING COMPOSER ;which composer >/dev/null && ( mv composer.phar $(which composer) ) || ( mv composer.phar /usr/bin/composer)  ;fi 
 	
-	)
+	) || (echo "NO COMPOSER DOWNLOADED" > /dev/stderr)
 
 ) & 
 
