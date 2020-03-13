bfcce058fa622d01f6e40dc0ce63decf7d755f0e
Your Name <you@example.com>
2020-03-13 18:05:25 +0100

    2020-03-13_17.05

---
+++ build/run-apache.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pidof apache2 apache2ctl|wc -c|grep ^0 -q && kill -7 $(pidof apache2 apache2ctl 2>/dev/null);
+grep apache /proc/$(cat /var/run/apache2/apache2.pid)/cmdline -q || rm /var/run/apache2/apache2.pid; 
+apache2ctl -DFOREGROUND;sleep 0.2
