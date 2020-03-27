9a9cd0816cc64a655634fab61c8ea6a448a4943b
Your Name <you@example.com>
2020-03-27 05:06:34 +0100

    2020-03-27_04.06

--- build/docker-install.sh
+++ build/docker-install.sh
@@ -48,7 +48,7 @@ _install_imagick() {
 		#pecl install imagick &&  
 		/bin/bash -c 'find /etc/php -type d -name "conf.d"  | while read phpconfdir ;do echo extension=imagick.so > $phpconfdir/20-imagick.ini;done' || true &
 		cd /tmp/ && wget https://pecl.php.net/get/imagick-3.4.3.tgz -O- -q |tar xvz && cd /tmp/imagick-3.4.3/  && phpize && ./configure && make -j $(nproc) && make install || exit 333
-		apt-get -y purge  libmagickwand-dev php${PHPVersion}-dev libjpeg-dev libpng-dev libwebp-dev || true  &
+		apt-get -y purge  libmagickwand-dev php${PHPVersion}-dev libjpeg-dev libpng-dev libwebp-dev || true  
 		find /tmp/ -type d -name "lilbwebp*" |xargs rm -rf || true & 
 		find /tmp/ -type d -name "ImageMagick*" |xargs rm -rf || true &
 		find /tmp/ -type d -name "imagick*" |xargs rm -rf || true &
