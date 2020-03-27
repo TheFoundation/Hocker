3d959d4d4728b18c5c87c9df0523e06335f932e0
Your Name <you@example.com>
2020-03-27 03:18:22 +0100

    2020-03-27_02.18

--- build/docker-install.sh
+++ build/docker-install.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
+
+##
+_do_cleanup_quick() { 
+			which apt-get 2>/dev/null && apt-get autoremove -y --force-yes &&  apt-get clean &&  rm /var/lib/apt/lists/*_*
+			
+			echo ; } ;
+			
+##########################################
+_do_cleanup() { 
+      PHPLONGVersion=$(php --version|head -n1 |cut -d " " -f2);
+      PHPVersion=${PHPLONGVersion:0:3};
+      ##### remove all packages named -dev or -dev: (e.g. mylib-dev:amd64 )
+      apt-get purge -y build-essential gcc make $( dpkg --get-selections|grep -v deinstall$|cut -f1|cut -d" " -f1|grep  -e \-dev: -e \-dev$ )
+      apt-get -y autoremove
+			find /tmp/ -mindepth 1 -type f |xargs rm || true 
+			find /tmp/ -mindepth 1 -type d |xargs rm || true 
+			##remove package managers
+			which apt-get 2>/dev/null && apt-get autoremove -y --force-yes &&  apt-get clean &&  rm /var/lib/apt/lists/*_*
+			##remove ssh host keys
+			for keyz in /etc/dropbear/dropbear_dss_host_key /etc/dropbear/dropbear_rsa_host_key /etc/dropbear/dropbear_ecdsa_host_key ;do test -f $keyz && rm $keyz;done 
+			
+			echo ; } ;
+
+
+
+
 _install_dropbear() {
 	apt-get update && apt-get install -y build-essential git zlib1g-dev || exit 111 
 	cd /tmp/ &&  git clone https://github.com/mkj/dropbear.git && cd dropbear && autoconf  &&  autoheader  && ./configure &&    make PROGRAMS="dropbear dbclient dropbearkey dropbearconvert " -j$(nproc)  &&  make install || exit 222
 	rm -rf /tmp/dropbear || true 
-
+  apt-get -y purge zlib1g-dev
+  _do_cleanup_quick
  echo ; } ; 
 
 _install_imagick() { 
@@ -27,7 +54,7 @@ _install_imagick() {
 		find /tmp/ -type d -name "ImageMagick*" |xargs rm -rf || true &
 		find /tmp/ -type d -name "imagick*" |xargs rm -rf || true &
 		php -r 'phpinfo();'|grep  ^ImageMagick|grep WEBP -q || exit 444
-		which apt-get 2>/dev/null && apt-get autoremove -y --force-yes &&  apt-get clean &&  rm /var/lib/apt/lists/*_*
+  _do_cleanup_quick
 
 		echo ; } ;
 
@@ -35,9 +62,9 @@ _install_php_nofpm() {
 		_install_php_basic ;
 		PHPLONGVersion=$(php --version|head -n1 |cut -d " " -f2);
 		PHPVersion=${PHPLONGVersion:0:3};
-		apt-get -y install --no-install-recommends  libapache2-mod-php${PHPVersion}
+		apt-get update && apt-get -y install --no-install-recommends  libapache2-mod-php${PHPVersion}
 				which apt-get 2>/dev/null && apt-get autoremove -y --force-yes &&  apt-get clean &&  rm /var/lib/apt/lists/*_*
-
+    _do_cleanup_quick
 					echo ; } ;
 
 _install_php_fpm() {
@@ -46,9 +73,9 @@ _install_php_fpm() {
 		PHPVersion=${PHPLONGVersion:0:3};
 		apt-get -y --no-install-recommends  install php${PHPVersion}-fpm
 		cd /tmp && wget http://mirrors.kernel.org/ubuntu/pool/multiverse/liba/libapache-mod-fastcgi/libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb && dpkg -i libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb &&  apt install -f && rm /tmp/libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb
-		apt-get -y install --no-install-recommends fcgiwrap apache2-utils php${PHPVersion}-fpm  php${PHPVersion}-fpm php${PHPVersion}-common libapache2-mod-fastcgi
+		apt-get update && apt-get -y install --no-install-recommends fcgiwrap apache2-utils php${PHPVersion}-fpm  php${PHPVersion}-fpm php${PHPVersion}-common libapache2-mod-fastcgi
 		(mkdir -p /etc/php/${PHPVersion}/cli/conf.d /etc/php/${PHPVersion}/fpm/conf.d /etc/php/${PHPVersion}/apache2/conf.d ;true)
-				which apt-get 2>/dev/null && apt-get autoremove -y --force-yes &&  apt-get clean &&  rm /var/lib/apt/lists/*_*
+    _do_cleanup_quick
 
 					echo ; } ;
 		
@@ -85,7 +112,7 @@ _install_php_basic() {
 				##MCRYPT ## was in php until 7.1
 		apt-get update && apt-get -y install php${PHPVersion}-dev && /bin/bash -c 'echo |pecl install redis' && echo extension=redis.so > /etc/php/${PHPVersion}/mods-available/redis.ini && phpenmod redis
 		which apt-get 2>/dev/null && apt-get autoremove -y --force-yes &&  apt-get clean &&  rm /var/lib/apt/lists/*_*
-
+    _do_cleanup_quick
 			echo ; } ;
 			
 ##########################################
@@ -113,7 +140,7 @@ _install_mariadb_ubuntu() {
 				add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirrors.n-ix.net/mariadb/repo/'$2'/ubuntu '$3' main' 
 				apt-get update && DEBIAN_FRONTEND=noninteractive	apt-get -y install --no-install-recommends mariadb-server mariadb-client
 				which apt-get 2>/dev/null && apt-get autoremove -y --force-yes &&  apt-get clean &&  rm /var/lib/apt/lists/*_*
-
+        _do_cleanup_quick ;
 				echo ; } ;
 
 _setup_wwwdata() {
@@ -124,28 +151,6 @@ _setup_wwwdata() {
 				ln -s /var/www/html /root/ &&  mkdir -p /var/www/.ssh /var/www/include /var/www/include_local && chown www-data /var/www/ -R && mkdir /root/.ssh && touch /root/.ssh/authorized_keys 
 				touch /var/www/.ssh/authorized_keys && chown root:root /var/www/.ssh /var/www/.ssh/authorized_keys && chmod go-rw  /root/.ssh/authorized_keys /root/.ssh /var/www/.ssh /var/www/.ssh/authorized_keys
 
-			echo ; } ;
-
-##
-_do_cleanup_quick() { 
-			which apt-get 2>/dev/null && apt-get autoremove -y --force-yes &&  apt-get clean &&  rm /var/lib/apt/lists/*_*
-			
-			echo ; } ;
-			
-##########################################
-_do_cleanup() { 
-      PHPLONGVersion=$(php --version|head -n1 |cut -d " " -f2);
-      PHPVersion=${PHPLONGVersion:0:3};
-      ##### remove all packages named -dev or -dev: (e.g. mylib-dev:amd64 )
-      apt-get purge -y build-essential gcc make $( dpkg --get-selections|grep -v deinstall$|cut -f1|cut -d" " -f1|grep  -e \-dev: -e \-dev$ )
-      apt-get -y autoremove
-			find /tmp/ -mindepth 1 -type f |xargs rm || true 
-			find /tmp/ -mindepth 1 -type d |xargs rm || true 
-			##remove package managers
-			which apt-get 2>/dev/null && apt-get autoremove -y --force-yes &&  apt-get clean &&  rm /var/lib/apt/lists/*_*
-			##remove ssh host keys
-			for keyz in /etc/dropbear/dropbear_dss_host_key /etc/dropbear/dropbear_rsa_host_key /etc/dropbear/dropbear_ecdsa_host_key ;do test -f $keyz && rm $keyz;done 
-			
 			echo ; } ;
 
 
