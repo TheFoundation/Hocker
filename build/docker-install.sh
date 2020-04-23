c259d9331274e074169cc7f6bad1c9ab09e2334d
Your Name <you@example.com>
2020-04-23 16:29:25 +0200

    2020-04-23_14.29

--- build/docker-install.sh
+++ build/docker-install.sh
@@ -46,7 +46,7 @@ _install_imagick() {
 		/bin/bash -c 'cd $(find /tmp/ -type d -name "ImageMagick-*" |head -n1) && ./configure  --with-webp=yes '"|sed 's/$/ → /g'|tr -d '\n' "' && make -j$(nproc) && make install && ldconfig /usr/local/lib &&  ( find /tmp/ -name "ImageMagic*" |xargs rm -rf  ) && identify -version  ' || exit 222
 		
 		##PHP-imagick
-		apt-get purge php-imagick	
+		apt-get -y purge php-imagick	
 		#pecl install imagick &&  
 		/bin/bash -c 'find /etc/php -type d -name "conf.d"  | while read phpconfdir ;do echo extension=imagick.so > $phpconfdir/20-imagick.ini;done' || true &
 		cd /tmp/ && wget https://pecl.php.net/get/imagick-3.4.3.tgz -O- -q |tar xvz && cd /tmp/imagick-3.4.3/  && phpize && ./configure && make -j $(nproc) && make -j3 install || exit 333
