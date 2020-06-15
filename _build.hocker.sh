1026343b7aab661d0bea801fc3c0505703d5edb5
Your Name <you@example.com>
2020-06-15 20:34:45 +0200

    2020-06-15-18.34

--- _build.hocker.sh
+++ _build.hocker.sh
@@ -404,8 +404,7 @@ which apk       2>/dev/null |grep -q apk && apk add git util-linux bash qemu-aar
   
 which git 2>/dev/null |grep -q git || which apt-get   2>/dev/null |grep -q apt-get && apt-get install -y git bash && apt-get -y install jq || true
 which apt-get   2>/dev/null |grep -q apt-get && apt-get install -y binfmt-support || true
-which apt-get   2>/dev/null |grep -q apt-get && apt-get install -y  qemu-user-binfmt || true
-which apt-get   2>/dev/null |grep -q apt-get && apt-get install -y  qemu-user-static || true
+which apt-get   2>/dev/null |grep -q apt-get && apt-get install -y  qemu-user-static || apt-get install -y  qemu-user-binfmt || true
 
 startdir=$(pwd)
 mkdir buildlogs
