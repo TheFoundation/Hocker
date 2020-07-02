fade649d053558cf4eeeb03bd2f29ab6df3b10d1
Your Name <you@example.com>
2020-07-02 20:57:25 +0200

    2020-07-02-18.57

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -7,6 +7,8 @@ CONF_DIR="/etc/dropbear"
 SSH_KEY_DSS="${CONF_DIR}/dropbear_dss_host_key"
 SSH_KEY_RSA="${CONF_DIR}/dropbear_rsa_host_key"
 SSH_KEY_ECDSA="${CONF_DIR}/dropbear_ecdsa_host_key"
+SSH_KEY_ED25519="${CONF_DIR}/dropbear_ecdsa_host_key"
+
 # Check if conf dir exists
 if [ ! -d ${CONF_DIR} ]; then
     mkdir -p ${CONF_DIR}
@@ -22,15 +24,23 @@ if [ ! -f ${SSH_KEY_RSA} ]; then
     chmod 600              ${SSH_KEY_RSA}
 fi & 
 
-# Check if keys exists
-if [ ! -f ${SSH_KEY_DSS} ]; then
-    dropbearkey  -t dss -f   ${SSH_KEY_DSS}
-    chown root:root          ${SSH_KEY_DSS}
-    chmod 600                ${SSH_KEY_DSS}
+
+## OpenSSH 7.0 and greater similarly disables the ssh-dss (DSA) public key algorithm. It too is weak and we recommend against its use. 
+rm ${SSH_KEY_DSS} 2>/dev/null || true &
+## Check if keys exists
+#if [ ! -f ${SSH_KEY_DSS} ]; then
+#    dropbearkey  -t dss -f   ${SSH_KEY_DSS};    chown root:root          ${SSH_KEY_DSS};    chmod 600                ${SSH_KEY_DSS} 
+#fi &
+
+if [ ! -f ${SSH_KEY_ED25519} ]; then
+    dropbearkey  -t ecdsa -f ${SSH_KEY_ED25519}
+    chown root:root          ${SSH_KEY_ED25519}
+    chmod 600                ${SSH_KEY_ED25519}
+
 fi &
 
 if [ ! -f ${SSH_KEY_ECDSA} ]; then
-    dropbearkey  -t ecdsa -f ${SSH_KEY_ECDSA}
+    dropbearkey  -t ecdsa -f ${SSH_KEY_ECDSA} -s 521
     chown root:root          ${SSH_KEY_ECDSA}
     chmod 600                ${SSH_KEY_ECDSA}
 
