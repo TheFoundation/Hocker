b0aa4513dfa54a31f570ac752ede7eaab960fcc9
Any Name <git@b1nch0.us.to>
2020-08-14 10:30:48 +0200

    2020-08-14-08.30

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -323,9 +323,12 @@ test -f /etc/apache2/sites-available/000-default.conf || cp /etc/apache2/sites-a
 find /etc/php/*/cli/ -name php.ini |while read php_cli_ini ;do sed 's/max_execution_time.\+/max_execution_time = 0 /g ' -i $php_cli_ini & done
 
 #raise upload limit for default 2M to 128M
+
+if [  -z "${MAX_UPLOAD_MB}" ] ; then
 find /etc/php/*/ -name php.ini |while read php_ini ;do sed 's/upload_max_filesize = 2M/upload_max_filesize = 128M /g;s/post_max_size.\+/post_max_size = 128M/g' -i $php_ini & done
 
-if [ -z "${MAX_UPLOAD_MB}" ] ; then
+else
+
 find /etc/php/*/ -name php.ini |while read php_ini ;do sed 's/upload_max_filesize = 2M/upload_max_filesize = '${MAX_UPLOAD_MB}'M /g;s/post_max_size.\+/post_max_size = '${MAX_UPLOAD_MB}'M/g' -i $php_ini & done
 
 fi
