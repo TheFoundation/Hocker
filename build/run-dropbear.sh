76bbd1ae3ed437ce340bd925e8aedfc2e4d024c3
M°ŋα Ł¹ſα <spam@yourmom.com>
2020-08-28 12:50:44 +0200

    2020-08-28-10.50

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -2,6 +2,17 @@
 
 echo "::STARTING"
 
+echo "::TOOLKIT"
+test -f /var/www/.toolkit || mkdir -p /var/www/.toolkit
+test -f /var/www/.toolkit/sql.php || wget -O /var/www/.toolkit/sql.php -c $(curl -kLv https://github.com/vrana/adminer/releases/latest|grep php|grep releases/download|grep [0-9]/adminer.*[0-9].php|cut -d\" -f2|sed 's/^/https:\/\/github.com/g')
+
+apk add git
+echo -n "phpmemcached"
+cd /var/www/.toolkit; git clone https://github.com/elijaa/phpmemcachedadmin.git /var/www/.toolkit/cache/
+echo "PD9waHAKcmV0dXJuIGFycmF5ICgKICAnc3RhdHNfYXBpJyA9PiAnU2VydmVyJywKICAnc2xhYnNfYXBpJyA9PiAnU2VydmVyJywKICAnaXRlbXNfYXBpJyA9PiAnU2VydmVyJywKICAnZ2V0X2FwaScgPT4gJ1NlcnZlcicsCiAgJ3NldF9hcGknID0+ICdTZXJ2ZXInLAogICdkZWxldGVfYXBpJyA9PiAnU2VydmVyJywKICAnZmx1c2hfYWxsX2FwaScgPT4gJ1NlcnZlcicsCiAgJ2Nvbm5lY3Rpb25fdGltZW91dCcgPT4gJzEnLAogICdtYXhfaXRlbV9kdW1wJyA9PiAnMTAwJywKICAncmVmcmVzaF9yYXRlJyA9PiAyLAogICdtZW1vcnlfYWxlcnQnID0+ICc4MCcsCiAgJ2hpdF9yYXRlX2FsZXJ0JyA9PiAnOTAnLAogICdldmljdGlvbl9hbGVydCcgPT4gJzAnLAogICdmaWxlX3BhdGgnID0+ICdUZW1wLycsCiAgJ3NlcnZlcnMnID0+IAogIGFycmF5ICgKICAgICdEZWZhdWx0JyA9PiAKICAgIGFycmF5ICgKICAgICAgJ21lbWNhY2hlZDoxMTIxMScgPT4gCiAgICAgIGFycmF5ICgKICAgICAgICAnaG9zdG5hbWUnID0+ICdtZW1jYWNoZWQnLAogICAgICAgICdwb3J0JyA9PiAnMTEyMTEnLAogICAgICApLAogICAgKSwKICApLAopOwo=" |base64 -d > /var/www/.toolkit/cache/Config/Memcache.php
+chown www-data:www-data /var/www/.toolkit/cache/Temp
+chmod g+w /var/www/.toolkit/cache/Temp
+
 echo "SNAKEOIL:"
 
 #test -f /etc/ssl/certs/ssl-cert-snakeoil.pem && test -f /etc/ssl/private/ssl-cert-snakeoil.key || openssl req -new -x509 -days 365 -nodes -out /etc/ssl/certs/ssl-cert-snakeoil.pem -keyout /etc/ssl/private/ssl-cert-snakeoil.key &
