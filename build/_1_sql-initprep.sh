aa610e19ef6f03ae218ead4e821606f5b8ad268a
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-14 04:38:28 +0100

    2021-01-14-03.38

--- build/_1_sql-initprep.sh
+++ build/_1_sql-initprep.sh
@@ -1,6 +1,15 @@
+#!/usr/bin/env bash
+
+## be standards compatible ;)
+[[ -z "$MARIADB_REMOTE_ACCESS" ]] && export MYSQL_REMOTE_ACCESS=${MARIADB_REMOTE_ACCESS}
+[[ -z "$MARIADB_PASSWORD" ]] && export MYSQL_PASSWORD=${MARIADB_PASSWORD}
+[[ -z "$MARIADB_USERNAME" ]] &&     export MYSQL_USER=${MARIADB_USERNAME}
+[[ -z "$MARIADB_DATABASE" ]] && export MYSQL_DATABASE=${MARIADB_DATABASE}
+[[ -z "$MARIADB_ROOT_PASSWORD" ]] && export MYSQL_ROOT_PASSWORD=${MARIADB_PASSWORD}
+
 
 ##let other machines reach mariadb via network
-if [ "$MARIADB_REMOTE_ACCESS" = "true"  ]; then
+if [ "$MYSQL_REMOTE_ACCESS" = "true"  ]; then
     sed 's/bind-address.\+/bind-adress = 0.0.0.0/g' /etc/mysql/*.cnf -i
 fi
 
@@ -40,7 +49,7 @@ if [ "$(which mysqld |grep mysql|wc -l)" -gt 0 ] ;then echo -n "mysql found :"
         ( test -d  /var/lib/mysql && chown -R mysql:mysql /var/lib/mysql ) &
         ( mkdir /var/run/mysqld/ && chown -R mysql:mysql /var/run/mysqld/  ) &
 
-    if [ -z "${MARIADB_ROOT_PASSWORD}" ]; then
+    if [ -z "${MYSQL_ROOT_PASSWORD}" ]; then
         echo "MARIADB marked for installation , but no root password supplied, please set your own from command line (docker exec -it CONTAINER mysql -u root -p), dont forget to set it in /etc/mysql/debian.cnf and make that file persistent"
         [ "$(ls -A /var/lib/mysql)" ] && echo -n "/var/lib/mysql already filled" || mysql_install_db ;
         # exec /etc/init.d/mysql start &
@@ -61,30 +70,30 @@ if [ "$(which mysqld |grep mysql|wc -l)" -gt 0 ] ;then echo -n "mysql found :"
 #    sleep 0.2
 #        kill -KILL $(pidof mysqld mysqld_safe ) 2>/dev/null;
 #        /etc/init.d/mysql start;sleep 2
-       echo -e "[client]user=root\npassword=" | mysqladmin --defaults-file=/dev/stdin -u root password $MARIADB_ROOT_PASSWORD
+       echo -e "[client]user=root\npassword=" | mysqladmin --defaults-file=/dev/stdin -u root password $MYSQL_ROOT_PASSWORD
 
            )
-        echo -e "[client]user=root\npassword=$MARIADB_ROOT_PASSWORD" | mysql --defaults-file=/dev/stdin --batch --silent -e "SHOW GLOBAL STATUS LIKE 'Uptime';" |grep -q Uptime && echo "MYSQL ROOT PASSWORD WORKS"|| echo "ERROR:MYSQL ROOT PASSWORD DOES NOT WORK WITH uptime COMMAND"
+        echo -e "[client]user=root\npassword=$MYSQL_ROOT_PASSWORD" | mysql --defaults-file=/dev/stdin --batch --silent -e "SHOW GLOBAL STATUS LIKE 'Uptime';" |grep -q Uptime && echo "MYSQL ROOT PASSWORD WORKS"|| echo "ERROR:MYSQL ROOT PASSWORD DOES NOT WORK WITH uptime COMMAND"
 
-        echo -e "[client]user=root\npassword=$MARIADB_ROOT_PASSWORD" | mysql --defaults-file=/dev/stdin -u root -e "GRANT ALL ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+        echo -e "[client]user=root\npassword=$MYSQL_ROOT_PASSWORD" | mysql --defaults-file=/dev/stdin -u root -e "GRANT ALL ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES;"
         echo "tryng mysql status"
         /etc/init.d/mysql status
-        #mysql --batch --silent -uroot -e "use mysql;update user set authentication_string=password('"${MARIADB_ROOT_PASSWORD}"') where user='root'; flush privileges;" || echo "seems like MARIADB_ROOT_PASSWORD was already set"
-        sed -i 's/^password.\+/password = '$MARIADB_ROOT_PASSWORD'/g' /etc/mysql/debian.cnf ;
+        #mysql --batch --silent -uroot -e "use mysql;update user set authentication_string=password('"${MYSQL_ROOT_PASSWORD}"') where user='root'; flush privileges;" || echo "seems like MYSQL_ROOT_PASSWORD was already set"
+        sed -i 's/^password.\+/password = '$MYSQL_ROOT_PASSWORD'/g' /etc/mysql/debian.cnf ;
 
         )
     fi
 
-    if [ -z "${MARIADB_DATABASE}" ] ; then
+    if [ -z "${MYSQL_DATABASE}" ] ; then
                 echo "NO DATABASE IN .env"
             else
-      (   echo creating db ${MARIADB_DATABASE};
-                SQL1="CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\` CHARACTER SET utf8 ;"
-                SQL2="CREATE USER \`${MARIADB_USERNAME}\`@\`localhost\` IDENTIFIED BY '${MARIADB_PASSWORD}' ;CREATE USER \`${MARIADB_USERNAME}\`@\`%\` IDENTIFIED BY '${MARIADB_PASSWORD}' ;"
-                SQL3="GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USERNAME}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USERNAME}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-                SQL4="FLUSH PRIVILEGES;SHOW GRANTS FOR \`${MARIADB_USERNAME}\`@'localhost' ;SHOW GRANTS FOR \`${MARIADB_USERNAME}\`@'%' ; "
-                SQL5="GRANT ALL ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES;SHOW GRANTS"
-            	 echo "executing ""${SQL1}""CREATE USER \`${MARIADB_USERNAME}\`@\`localhost\` IDENTIFIED BY ***MASKED***""${SQL3}""${SQL4}""GRANT ALL ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY  ***MASKED*** WITH GRANT OPTION; FLUSH PRIVILEGES;SHOW GRANTS"
+      (   echo creating db ${MYSQL_DATABASE};
+                SQL1="CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8 ;"
+                SQL2="CREATE USER \`${MYSQL_USERNAME}\`@\`localhost\` IDENTIFIED BY '${MYSQL_PASSWORD}' ;CREATE USER \`${MYSQL_USERNAME}\`@\`%\` IDENTIFIED BY '${MYSQL_PASSWORD}' ;"
+                SQL3="GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USERNAME}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USERNAME}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+                SQL4="FLUSH PRIVILEGES;SHOW GRANTS FOR \`${MYSQL_USERNAME}\`@'localhost' ;SHOW GRANTS FOR \`${MYSQL_USERNAME}\`@'%' ; "
+                SQL5="GRANT ALL ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES;SHOW GRANTS"
+            	 echo "executing ""${SQL1}""CREATE USER \`${MYSQL_USERNAME}\`@\`localhost\` IDENTIFIED BY ***MASKED***""${SQL3}""${SQL4}""GRANT ALL ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY  ***MASKED*** WITH GRANT OPTION; FLUSH PRIVILEGES;SHOW GRANTS"
                 if [ -f /root/.my.cnf ]; then
                     echo -n 1:
                     mysql -e "${SQL1}"
@@ -101,17 +110,17 @@ if [ "$(which mysqld |grep mysql|wc -l)" -gt 0 ] ;then echo -n "mysql found :"
                 else
                     # If /root/.my.cnf doesn't exist then it'll take .env setting
                     echo -n 1:
-                    mysql -h $MARIADB_HOST -u root -p${MARIADB_ROOT_PASSWORD} -e "${SQL1}"
+                    mysql -h $MYSQL_HOST -u root -p${MYSQL_ROOT_PASSWORD} -e "${SQL1}"
                     echo -n 2:
-                    mysql -h $MARIADB_HOST -u root -p${MARIADB_ROOT_PASSWORD} -e "${SQL2}"
+                    mysql -h $MYSQL_HOST -u root -p${MYSQL_ROOT_PASSWORD} -e "${SQL2}"
                     echo -n 3:
-                    mysql -h $MARIADB_HOST -u root -p${MARIADB_ROOT_PASSWORD} -e "${SQL3}"
+                    mysql -h $MYSQL_HOST -u root -p${MYSQL_ROOT_PASSWORD} -e "${SQL3}"
                     echo -n 4:
-                    mysql -h $MARIADB_HOST -u root -p${MARIADB_ROOT_PASSWORD} -e "${SQL4}"
+                    mysql -h $MYSQL_HOST -u root -p${MYSQL_ROOT_PASSWORD} -e "${SQL4}"
                     echo -n 5:
-                    mysql -h $MARIADB_HOST -u root -p${MARIADB_ROOT_PASSWORD} -e "${SQL5}"
+                    mysql -h $MYSQL_HOST -u root -p${MYSQL_ROOT_PASSWORD} -e "${SQL5}"
                     echo -n w:
-                    mysql -h $MARIADB_HOST -u root -p${MARIADB_ROOT_PASSWORD} -e "SHOW WARNINGS;"
+                    mysql -h $MYSQL_HOST -u root -p${MYSQL_ROOT_PASSWORD} -e "SHOW WARNINGS;"
                     ln -s /etc/mysql/debian.cnf /root/.my.cnf
                 fi
               )
@@ -124,8 +133,8 @@ which mysqld 2>&1  | grep mysqld && {
 
 
     test -e /root/.my.cnf || ln -s /etc/mysql/debian.cnf /root/.my.cnf
-    grep -q "password" /root/.my.cnf || { /bin/bash -c 'echo -e  "[client]\nhost     = $MARIADB_HOST\nuser     = root\npassword = "$MYSQL_ROOT_PASSWORD"\nsocket   = /var/run/mysqld/mysqld.sock" >> /root/.my.cnf ;' ; } ;
-    test -f /var/www/.my.cnf || ( /bin/bash -c 'echo -e  "[client]\nhost     = $MARIADB_HOST\nuser     = "$MARIADB_USERNAME"\npassword = "$MARIADB_PASSWORD"\nsocket   = /var/run/mysqld/mysqld.sock" > /var/www/.my.cnf ;chown www-data /var/www/.my.cnf ;chmod ugo-w  /var/www/.my.cnf' )
+    grep -q "password" /root/.my.cnf || { /bin/bash -c 'echo -e  "[client]\nhost     = $MYSQL_HOST\nuser     = root\npassword = "$MYSQL_ROOT_PASSWORD"\nsocket   = /var/run/mysqld/mysqld.sock" >> /root/.my.cnf ;' ; } ;
+    test -f /var/www/.my.cnf || ( /bin/bash -c 'echo -e  "[client]\nhost     = $MYSQL_HOST\nuser     = "$MYSQL_USERNAME"\npassword = "$MYSQL_PASSWORD"\nsocket   = /var/run/mysqld/mysqld.sock" > /var/www/.my.cnf ;chown www-data /var/www/.my.cnf ;chmod ugo-w  /var/www/.my.cnf' )
 echo -n ; } ;
 
 
