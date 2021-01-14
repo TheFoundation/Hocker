ae38228040e742818aa9394309a05724ba0aa537
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-14 07:09:19 +0100

    2021-01-14-06.09

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -131,7 +131,8 @@ wait
   while (true);do
     for artisanfile in $(ls /var/www/html/artisan /var/www/$(hostname -f)/ /var/www/*/artisan -1 2>/dev/null|grep -v  -e "\.bak/artisan" -e "OLD/artisan" -e  "old/artisan"  |head -n1 ) ;do
         CRONCMD='*/10 * * * * /usr/bin/php '${artisanfile}' schedule:run &>/dev/shm/cron_artisan.sched.log'
-        grep '/usr/bin/php '${artisanfile}' schedule:run ' /var/spool/cron/crontabs/www-data  || ( (echo ;echo "${CRONCMD}" )  |tee -a /var/spool/cron/crontabs/www-data ;
+        #grep '/usr/bin/php '${artisanfile}' schedule:run ' /var/spool/cron/crontabs/www-data  || ( (echo ;echo "${CRONCMD}" )  |tee -a /var/spool/cron/crontabs/www-data ;
+        crontab -l -u www-data 2>/dev/null | grep -q '/usr/bin/php '${artisanfile}' schedule:run '  || { (crontab -l -u www-data 2>/dev/null; echo "${CRONCMD}") | crontab -u www-data - ; } ;
         which supervisorctl 2>&1 | grep -q supervisorctl && supervisorctl restart cron
         which supervisorctl 2>&1 | grep -q supervisorctl || service cron restart)
     sleep 120;
