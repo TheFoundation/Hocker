9302d9f9a76a65342646c3e338debcaefc31f90d
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-15 17:01:09 +0100

    2021-01-15-16.01

--- build/run-apache.sh
+++ build/run-apache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 [[ -z $(pidof apache2 apache2ctl|wc -c|grep ^0 -q && kill -7 $(pidof apache2 apache2ctl 2>/dev/null); ) ]] || grep apache /proc/$(cat /var/run/apache2/apache2.pid)/cmdline -q || rm /var/run/apache2/apache2.pid;
-apache2ctl -DFOREGROUND  2>/var/log/apache2/access.log /var/log/apache2/error.log;sleep 0.2
+apache2ctl -DFOREGROUND  2>/var/log/apache2/access.log >/var/log/apache2/error.log;sleep 0.2
