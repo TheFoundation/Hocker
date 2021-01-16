1030ff2a9e75b369262bf9b5dfb0c496b9319094
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-16 17:32:37 +0100

    2021-01-16-16.32

--- build/docker-install.sh
+++ build/docker-install.sh
@@ -25,10 +25,9 @@ PHPLONGVersion=$(php -r'echo PHP_VERSION;')
 PHPVersion=$(echo $PHPLONGVersion|sed 's/^\([0-9]\+.[0-9]\+\).\+/\1/g');
 echo "removing unwanted php versions"
 removeselector=$(dpkg --get-selections|grep ^php[0-9]|grep -v deinstall|cut -f1|grep -v ^php${PHPVersion})
-[[ -z "${removeselector}" ]] || { echo "removing pkgs:${removeselector}"; apt-get -y remove ${removeselector} } ;
+[[ -z "${removeselector}" ]] || { echo "removing pkgs:${removeselector}"; apt-get -y remove ${removeselector} ; } ;
 removeselector=$(find /etc/php -mindepth 1 -maxdepth 1 -type d |grep -v ${PHPVersion}$)
 find  $removeselector -delete
-
 echo ; } ;
 
 _build_pecl() {
