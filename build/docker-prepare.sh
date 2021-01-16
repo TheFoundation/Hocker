5a10e580746f31c3f86c376365089a610bb61e57
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-16 23:48:40 +0100

    2021-01-16-22.48

--- build/docker-prepare.sh
+++ build/docker-prepare.sh
@@ -24,7 +24,7 @@ _do_cleanup() {
         ##### remove all packages named *-dev* or *-dev:* (e.g. mylib-dev:amd64 )
 
         removeselector=$( dpkg --get-selections|grep -v deinstall$|cut -f1|cut -d" " -f1|grep -e python-software-properties -e software-properties-common  -e ^make -e ^build-essential -e \-dev: -e \-dev$ -e ^texlive-base -e  ^doxygen  -e  ^libllvm   -e ^gcc -e ^g++ -e ^build-essential -e \-dev: -e \-dev$ )
-        [[ -z "${removeselector}" ]] || apt-get purge -y ${removeselector} 2>&1 | sed 's/$/|/g'|tr -d '\n'
+        [[ -z "${removeselector}" ]] || { echo "deleting ${removeselector} " ; apt-get purge -y ${removeselector} 2>&1 | sed 's/$/|/g'|tr -d '\n' ; } ;
         which apt-get &>/dev/null && apt-get autoremove -y --force-yes 2>&1 | sed 's/$/|/g'|tr -d '\n'
         remove doc and man and /tmp
         deleteselector=$( find /tmp/  /var/cache/man     /usr/share/texmf/ /usr/local/share/doc /usr/share/doc /usr/share/man -mindepth 1 -type f 2>/dev/null  ) &
