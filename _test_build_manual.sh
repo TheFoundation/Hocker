a4969bf5962132103f81b8d278c7bf303476ea9f
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-17 05:07:12 +0100

    2021-01-17-04.07

--- _test_build_manual.sh
+++ _test_build_manual.sh
@@ -1,5 +1,12 @@
 cd build ;
 
 ( time docker build . -f Dockerfile-php${1}-dropbear-fpm  -t thefoundation/hocker:php${1}-dropbear-fpm 2>&1  && {
- docker run -v $(pwd)/../thefoundation-imagetester.sh:/_image_tests.sh --rm -t thefoundation/hocker:php${1}-dropbear-fpm /bin/bash /_image_tests.sh 2>&1  ) |tee /dev/shm/imagetest.$1.log   ; } ;
+ docker run \
+ -e APT_HTTP_PROXY_URL=${APT_HTTP_PROXY_URL} \
+ -e MAIL_HOST=${MAIL_HOST} \
+ -e APP_URL=${APP_URL} \
+ -e MAIL_USERNAME=${MAIL_USERNAME} \
+ -e MAIL_PASSWORD=${MAIL_PASSWORD} \
+ -v $(pwd)/../thefoundation-imagetester.sh:/_image_tests.sh \
+ --rm -t thefoundation/hocker:php${1}-dropbear-fpm /bin/bash /_image_tests.sh 2>&1  ) |tee /dev/shm/imagetest.$1.log   ; } ;
  echo "log in /dev/shm/imagetest.$1.log , length "$(wc -l /dev/shm/imagetest.$1.log)" lines"
