bc2d151d0c7562a9fc7a4b5e6c0eb557689c7106
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-17 03:13:46 +0100

    2021-01-17-02.13

--- thefoundation-imagetester.sh
+++ thefoundation-imagetester.sh
@@ -16,7 +16,8 @@ echo "docker CI test started at "$(date) | green
 sleep 2
 while  ( supervisorctl status 2>&1 | grep -i -e mysql -e fpm -e mariadb -e dropbear -e openssh -e nginx -e apache -e redis -e mongo |grep  -qv "RUNNING "  )   ;do
   [[ $(($(date -u +%s)-${start})) -gt 120 ]] && exit 999
-      echo -ne "init:waiting since "$(($(date -u +%s)-${start}))" seconds "|red ;echo -ne $(tail -n2 /dev/shm/startlog|tail -c 84  |tr -d '\r\n' ) '\r';sleep 2; done
+      echo -ne "init:waiting since "$(($(date -u +%s)-${start}))" seconds "|red ;echo -ne $(tail -n2 /dev/shm/startlog|tail -c 84  |tr -d '\r\n' ) '\r';sleep 2;
+    done
 
 
       which apachectl && {
