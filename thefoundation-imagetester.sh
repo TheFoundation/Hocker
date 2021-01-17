473b453b71537c4e97e213ec6a7d5635cdecbab2
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-17 15:53:19 +0100

    2021-01-17-14.53

--- thefoundation-imagetester.sh
+++ thefoundation-imagetester.sh
@@ -94,7 +94,8 @@ echo "PHP:"$(php --version|cut -d" " -f2) | yellow
 echo -n "PHP_CLI_MODULES:"|purple ;
 phpcliinfo=$(php -r 'phpinfo();')
 for modtest in ${phpmoduleswanted} ;do
-  echo "$phpcliinfo"|grep -i "${modtest}\.ini" -q  || { build_ok=no ;fail_reasons=${fail_reasons}" php_cli_phpinfo_grep_$modtest" ;echo -n " $modtest";echo -n ":FAIL"|red; } ;
+  echo "$phpcliinfo"|grep -i "${modtest}\.ini" -q  || { build_ok=no ;fail_reasons=${fail_reasons}" php_cli_phpinfo_grep_$modtest" ;
+                                                          echo -n " $modtest";echo -n ":FAIL"|red; } ;
   echo "$phpcliinfo"|grep  "${modtest}\.ini"   -q  && {   echo -n " $modtest" ; echo -n ":OK"|blue ; } ;
 done
 
@@ -107,8 +108,9 @@ curl_result=$(curl -kLv https://127.0.0.1/phi.php 2>/dev/shm/curl_ERR_log)
 
 for modtest in ${phpmoduleswanted};do
 
-  echo "$curl_result"|grep  "${modtest}\.ini" -q  || { build_ok=no ;fail_reasons=${fail_reasons}" php_curl_phpinfo_grep_$modtest" ; echo -n " $modtest" ; echo -n ":FAIL"|red ; } ;
-  echo "$curl_result"|grep  "${modtest}\.ini" -q  && {   echo -n " $modtest" ; echo -n ":OK"|blue ; } ;
+  echo "$curl_result"|grep  "${modtest}\.ini" -q  || { build_ok=no ;fail_reasons=${fail_reasons}" php_curl_phpinfo_grep_$modtest" ;
+                                                          echo -n " $modtest" ; echo -n ":FAIL"|red ; } ;
+  echo "$curl_result"|grep  "${modtest}\.ini" -q  && {    echo -n " $modtest" ; echo -n ":OK"|blue ; } ;
 done
 
 echo
