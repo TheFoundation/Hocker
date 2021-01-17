22f772a3fe26e868b3138d85bcc2bbf2e603e49e
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-17 02:40:44 +0100

    2021-01-17-01.40

--- thefoundation-imagetester.sh
+++ thefoundation-imagetester.sh
@@ -34,7 +34,7 @@ echo "##########"
 echo -n "APACHE MODULES:" | green
   apache_modules=$(apachectl -M 2>/dev/null)
   for term in headers ssl remoteip redirect actions fastcgi proxy_fcgi proxy_http proxy_wstunnel mpm_prefork ;do
-    echo "${apache_modules}" |sed 's/(shared)//g'| grep "${term}_module" || { build_ok=no ;fail_reasons=${fail_reasons}" apache_mod_${term}" ;echo FAIL |red; }
+    echo "${apache_modules}" |sed 's/(shared)//g'| grep "${term}_module" || { build_ok=no ;fail_reasons=${fail_reasons}" apache_mod_${term}" ;echo FAIL( $term ) |red; }
   done |tr -d '\n'
 
 echo ; } ;
