eee8efd00c76732dc9e6d73d4b6981249ebacad0
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-18 05:11:41 +0100

    2021-01-18-04.11

--- build/_1_sql-initprep.sh
+++ build/_1_sql-initprep.sh
@@ -59,7 +59,7 @@ if [ "$(which mysqld |grep mysql|wc -l)" -gt 0 ] ;then echo -n "mysql found :"
          (	[ "$(ls /var/lib/mysql/mysql/user*)" ] && echo -n " /var/lib/mysql already filled" || mysql_install_db ;
         #mysqld_safe --skip-grant-tables &  sleep 3;
         /etc/init.d/mysql start
-        sleep 2
+        sleep 5
         no_passwd_set=no
         echo -n "trying our root password from env"
         echo -e "[client]user=root\npassword=" | mysql --defaults-file=/dev/stdin --batch --silent -e "SHOW GLOBAL STATUS LIKE 'Uptime';" |grep -q Uptime && no_passwd_set=yes
