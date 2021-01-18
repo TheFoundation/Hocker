45a4c2ca0e6cb850e3d82dd0e4d0ab5cdd1406c2
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-18 20:10:44 +0100

    2021-01-18-19.10

--- build/_1_php-initprep.sh
+++ build/_1_php-initprep.sh
@@ -112,9 +112,9 @@ which apachectl && (
 # sed 's/CustomLog \/dev\/stdout/CustomLog ${APACHE_LOG_DIR}\/access.log/g' -i /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/default-ssl.conf ;
 #  sed 's/ErrorLog \/dev\/stdout/ErrorLog ${APACHE_LOG_DIR}\/error.log/g'    -i /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/default-ssl.conf ;
 
-sed 's/.\+/access.log/AccessLog \/dev\/stdout/g'  -i /etc/apache2/sites-enabled/*.conf  ;
-sed 's/.\+\/access.log/CustomLog \/dev\/stdout/g' -i /etc/apache2/sites-enabled/*.conf  ;
-sed 's/.\+\/error.log/ErrorLog \/dev\/stderr/g'   -i /etc/apache2/sites-enabled/*.conf  ;
+sed 's/.\+/access.log/AccessLog | /bin/bash /_3_logfilter_apache.sh >> \/dev\/stdout/g'  -i /etc/apache2/sites-enabled/*.conf  ;
+sed 's/.\+\/access.log/CustomLog | /bin/bash /_3_logfilter_apache.sh >> \/dev\/stdout/g' -i /etc/apache2/sites-enabled/*.conf  ;
+sed 's/.\+\/error.log/ErrorLog | /bin/bash /_3_logfilter_apache.sh >> \/dev\/stderr/g'   -i /etc/apache2/sites-enabled/*.conf  ;
 if [ -z "${MAIL_ADMINISTRATOR}" ];
         then echo "::MAIL_ADMINISTRATOR not set FIX THIS !(apache ServerAdmin)"
         else sed 's/ServerAdmin webmaster@localhost/ServerAdmin '${MAIL_ADMINISTRATOR}'/g' -i /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/default-ssl.conf
