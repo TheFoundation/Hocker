4c9c6dd664e16d0b31b2e722ac2412bce0e0fbd3
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-18 20:25:33 +0100

    2021-01-18-19.25

--- build/_3_logfilter_apache.sh
+++ build/_3_logfilter_apache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #filter_web_log() {
-cat | grep --line-buffered -v -e 'StatusCabot' -e '"cabot/' -e '"HEAD / HTTP/1.1" 200 - "-" "curl/' -e "UptimeRobot/" -e "docker-health-check/over9000" -e "/favicon.ico"
+[[ "$1" = "err"]] && { cat | grep --line-buffered -v -e 'StatusCabot' -e '"cabot/' -e '"HEAD / HTTP/1.1" 200 - "-" "curl/' -e "UptimeRobot/" -e "docker-health-check/over9000" -e "/favicon.ico" |red   ; } ;
+[[ "$1" = "err"]] || { cat | grep --line-buffered -v -e 'StatusCabot' -e '"cabot/' -e '"HEAD / HTTP/1.1" 200 - "-" "curl/' -e "UptimeRobot/" -e "docker-health-check/over9000" -e "/favicon.ico" |green ; } ;
  # ; } ;
