396f6938bbb37140218048e6b74a29b4e213f283
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-18 20:30:35 +0100

    2021-01-18-19.30

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -354,7 +354,7 @@ done
 ##
 ##
 
-( sleep 30 ;echo " sys.info   |spawning service loop"|green ;service_loop ) &
+( sleep 30 ;echo " sys.info  |spawning service loop"|green ;service_loop ) &
 ##bash dislikes this as a function
 #                  _supervisor_logger_err() { sed 's/^[[:digit:]]\{4\}-[[:digit:]]\{2\}-[[:digit:]]\{2\} [[:digit:]]\{2\}:[[:digit:]]\{2\}:[[:digit:]]\{2\},[[:digit:]]\{3\} [[:upper:]]/  sys.err   |\0/g' ; } ;
 #                  _supervisor_logger_std() { sed 's/^[[:digit:]]\{4\}-[[:digit:]]\{2\}-[[:digit:]]\{2\} [[:digit:]]\{2\}:[[:digit:]]\{2\}:[[:digit:]]\{2\},[[:digit:]]\{3\} [[:upper:]]/ sys.info   |\0/g' ; } ;
