636631c0e406cc06104dcdd690c5aeba1f6cb8a9
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-18 01:55:09 +0100

    2021-01-18-00.55

--- build/run-apache.sh
+++ build/run-apache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 filter_web_log() { grep --line-buffered -v -e 'StatusCabot' -e '"cabot/' -e '"HEAD / HTTP/1.1" 200 - "-" "curl/' -e "UptimeRobot/" -e "docker-health-check/over9000" -e "/favicon.ico" ; } ;
 
-[[ -z $(pidof apache2 apache2ctl|wc -c|grep ^0 -q || kill -7 $(pidof apache2 apache2ctl 2>/dev/null); ) ]] || grep apache /proc/$(cat /var/run/apache2/apache2.pid)/cmdline -q || rm /var/run/apache2/apache2.pid;
+[[ -z $(pidof apache2 apache2ctl)  ]] || { kill $(pidof apache2 apache2ctl) ; } ;
+grep apache /proc/$(cat /var/run/apache2/apache2.pid)/cmdline -q || rm /var/run/apache2/apache2.pid;
 apache2ctl -DFOREGROUND  2>/dev/stderr | filter_web_log >/dev/stdout;sleep 0.2
