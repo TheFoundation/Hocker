2ff39b6a990f19da1e38dc324fee9c6e898123a9
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-18 20:42:22 +0100

    2021-01-18-19.42

--- build/run-apache.sh
+++ build/run-apache.sh
@@ -2,6 +2,7 @@
 
 
 [[ -z $(pidof apache2 apache2ctl)  ]] || { kill $(pidof apache2 apache2ctl) ; } ;
+sleep 1;
 grep apache /proc/$(cat /var/run/apache2/apache2.pid 2>/dev/null)/cmdline 2>/dev/null -q || rm /var/run/apache2/apache2.pid 2>/dev/null;
 echo apache:starting
 #apache2ctl -DFOREGROUND  2>/dev/stderr | filter_web_log >/dev/stdout;sleep 0.2
