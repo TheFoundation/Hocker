90cbf42da6bc5306ac505baf70eb75aba0ff2e28
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-19 16:05:58 +0100

    2021-01-19-15.05

--- build/_1_php-initfpm.sh
+++ build/_1_php-initfpm.sh
@@ -1,9 +1,37 @@
-#!/bin/bash 
+#!/bin/bash
 
 PHPLONGVersion=$(php --version|head -n1 |cut -d " " -f2);
 PHPVersion=${PHPLONGVersion:0:3};
 
+grep ^'php_admin_value\[disable_functions\]'  /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf  ||  {
 
+  ## typo3 needs exec     sometimes _> /var/www/typo3_src/
+  ## laravel needs pcntl_async_signals with redis/horizon
+  #exec,passthru,shell_exec,system,proc_open,popen,curl_exec,curl_multi_exec,parse_ini_file,show_source
+  #e.g. php_admin_value[disable_functions] = apache_child_terminate,apache_setenv
+  FORBIDDEN_FUNCTIONS_ALL="apache_child_terminate,apache_setenv,define_syslog_variables,escapeshellarg,escapeshellcmd,eval,exec,fp,fput,ftp_connect,ftp_exec,ftp_get,ftp_login,ftp_nb_fput,ftp_put,ftp_raw,ftp_rawlist,highlight_file,ini_alter,ini_get_all,ini_restore,inject_code,mysql_pconnect,openlog,passthru,pcntl_alarm,pcntl_exec,pcntl_fork,pcntl_get_last_error,pcntl_getpriority,pcntl_setpriority,pcntl_signal,pcntl_signal_dispatch,pcntl_sigprocmask,pcntl_sigtimedwait,pcntl_sigwaitinfo,pcntl_strerror,pcntl_wait,pcntl_waitpid,pcntl_wexitstatus,pcntl_wifcontinued,pcntl_wifexited,pcntl_wifsignaled,pcntl_wifstopped,pcntl_wstopsig,pcntl_wtermsig,phpAds_XmlRpc,phpAds_remoteInfo,phpAds_xmlrpcDecode,phpAds_xmlrpcEncode,popen,posix_getpwuid,posix_kill,posix_mkfifo,posix_setpgid,posix_setsid,posix_setuid,posix_uname,proc_close,proc_get_status,proc_nice,proc_open,proc_terminate,shell_exec,syslog,system,xmlrpc_entity_decode"
+  FORBIDDEN_FUNCTIONS_HARDENED="exec,passthru,shell_exec,system,proc_open,popen,parse_ini_file,show_source,chroot,escapeshellcmd,escapeshellarg,shell_exec,proc_open,proc_get_status,ini_restore,ftp_connect,ftp_exec,ftp_get,ftp_login,ftp_nb_fput,ftp_put,ftp_raw"
+  FORBIDDEN_FUNCTIONS_DEFAULTS="system,passthru,shell_exec,chrootpopen,parse_ini_file,show_source,chroot,shell_exec,ini_restore,apache_child_terminate,apache_setenv,ftp_connect,ftp_exec,ftp_get,ftp_login,ftp_nb_fput,ftp_put,ftp_raw"
+  FORBIDDEN_FUNCTIONS_SELECTED=""
+  [[ "$PHP_FORBIDDEN_FUNCTIONS" = "NONE" ]] &&  FORBIDDEN_FUNCTIONS_SELECTED=""
+  [[ "$PHP_FORBIDDEN_FUNCTIONS" = "HARDENED" ]] &&  FORBIDDEN_FUNCTIONS_SELECTED="${FORBIDDEN_FUNCTIONS_HARDENED}"
+  [[ "$PHP_FORBIDDEN_FUNCTIONS" = "ALL" ]] &&  FORBIDDEN_FUNCTIONS_SELECTED="${FORBIDDEN_FUNCTIONS_ALL}"
+  [[ -z "$PHP_FORBIDDEN_FUNCTIONS"  ]] &&  FORBIDDEN_FUNCTIONS_SELECTED=${FORBIDDEN_FUNCTIONS_DEFAULTS}
+
+  #if entered directly...
+  echo "$PHP_FORBIDDEN_FUNCTIONS" |grep "," && FORBIDDEN_FUNCTIONS_SELECTED=${PHP_FORBIDDEN_FUNCTIONS}
+  [[ -z "$FORBIDDEN_FUNCTIONS_SELECTED" ]] &&  [[ "NONE" =  "${PHP_FORBIDDEN_FUNCTIONS}" ]] &&  echo "ERROR: set PHP_FORBIDDEN_FUNCTIONS to 'NONE' if you want to dangerously enable everything , it is currently: "${PHP_FORBIDDEN_FUNCTIONS}
+  fpmfile=/etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf
+  sed 's///g'
+  grep " ${FORBIDDEN_FUNCTIONS_DEFAULTS}" "${fpmfile}" && { echo "OK     php_forbidden_functions_default found in" "${fpmfile}"  ; } ;
+  grep " ${FORBIDDEN_FUNCTIONS_DEFAULTS}" "${fpmfile}" || { echo "NOT OK php_forbidden_functions enforced ${fpmfile}  TO= ${FORBIDDEN_FUNCTIONS_DEFAULTS}" ;
+                                                          ##remove others
+                                                          sed 's/.\+php_admin_value.disable_functions.\+//g' "${fpmfile}" -i
+                                                          ##write
+                                                          (echo;echo "php_admin_value[disable_functions] = "${FORBIDDEN_FUNCTIONS_DEFAULTS}) >> ${fpmfile}
+                                                        echo -n ; } ;
+  echo " sys.info  | PHP_DISABLE_FUNCTIONS:" $(grep )
+echo -n "=fpm" ; } ;
 
 
     echo " sys.info  | PHP_FPM::SESSIONS:"
