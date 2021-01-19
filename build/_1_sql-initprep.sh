ec2955beeecdb56e374858d46253e2f64917dfd1
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-19 19:19:29 +0100

    2021-01-19-18.19

--- build/_1_sql-initprep.sh
+++ build/_1_sql-initprep.sh
@@ -6,7 +6,7 @@ test -d /var/lib/mysql || echo " init.sql | SOFTFAIL: /var/lib/mysql not present
 grep  /var/lib/mysql /etc/mtab -q || echo " init.sql | SOFTFAIL: /var/lib/mysql not a volume "
 test -d /var/lib/mysql || mkdir -p /var/lib/mysql
 
-[[ -z "$MARIADB_HOST" ]]          && [[ -z "$MYSQL_HOST" ]]   && MARIADB_HOST=127.0.0.1
+[[ -z "$MARIADB_HOST" ]]          && [[ -z "$MYSQL_HOST" ]]   && MYSQL_HOST=127.0.0.1
 ## be standards compatible ;)
 [[ -z "$MARIADB_HOST" ]]          && [[ -z "$MYSQL_HOST" ]]          ||   export MARIADB_HOST=${MYSQL_HOST}
 [[ -z "$MARIADB_USERNAME" ]]      && [[ -z "$MYSQL_USER" ]]          ||   export MARIADB_USER=${MYSQL_USERNAME}
