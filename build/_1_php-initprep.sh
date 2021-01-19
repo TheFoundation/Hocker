965c90217b26512c959053884f0ce0036c8bff27
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-19 02:19:27 +0100

    2021-01-19-01.19

--- build/_1_php-initprep.sh
+++ build/_1_php-initprep.sh
@@ -182,8 +182,8 @@ if [ "$(ls -1 /usr/sbin/php-fpm* 2>/dev/null|wc -l)" -eq 0 ];then
         [[ "yes" = "${setup_redis}" ]]       && (
         echo "setting up redis sessionstorage";
         for phpconf in $(find $(find /etc/ -maxdepth 1 -name "php*") -name php.ini |grep -e apache -e fpm);do
-           sed 's/.\+session.save_\(handler\|path\).\+//g' ${phpconf} -i
-           ( echo '[Session]';echo "session.save_handler = redis" ; echo 'session.save_path = "'${PHP_SESSION_REDIS_HOST}'"' ) > ${fpmconf} ;
+            sed 's/.\+session.save_\(handler\|path\).\+//g' ${phpconf} -i
+            ( echo '[Session]';echo "session.save_handler = redis" ; echo 'session.save_path = "'${PHP_SESSION_REDIS_HOST}'"' ) > ${fpmconf} ;
          done
          echo -n; } ;
 
