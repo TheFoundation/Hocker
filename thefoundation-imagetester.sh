bffa7e9d7b7be2e96ab4a7d0cad957569db81e6c
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-19 13:32:47 +0100

    2021-01-19-12.32

--- thefoundation-imagetester.sh
+++ thefoundation-imagetester.sh
@@ -171,6 +171,8 @@ echo $(echo " | sendmail: " which sendmail;echo " | msmtp: ";which msmtp ;echo "
 ### see if the configs have sendmail_path
 mail_setting_found=false
 
+for configtype in $(find /etc/php/* -type d -name cli ; find /etc/php/* -type d -name fpm ; find /etc/php/* -type d -name apache2);do
+
 for configdir in $(find /etc/php/ -type d -name cli);do
   test -d ${configdir}/conf.d && grep "^sendmail_path" ${configdir}/conf.d/30-php-mail.ini |grep -q "/usr/bin/msmtp -t" || { build_ok=no ;fail_reasons=${fail_reasons}" sendmail_path" ;
                                                                                                                          echo "FAIL(sendmail_path ${clidir}/conf.d )" | red   ; }
@@ -180,10 +182,7 @@ done
 for conffile in $(find /etc/php* -name 30-php-mail.ini );do
   grep "^sendmail_path" ${conffile} |grep -q "/usr/bin/msmtp -t" || { build_ok=no ;fail_reasons=${fail_reasons}" sendmail_path" ;
                                                                                                                          echo "FAIL(sendmail_path ${clidir}/conf.d )" | red   ; }
-  grep "^sendmail_path" ${conffile} |grep -q "/usr/bin/msmtp -t" || {  echo "OK " ; mail_setting_found=yes   ; }
-echo -n; } ;
-
-
+  grep "^sendmail_path" ${conffile} |grep -q "/usr/bin/msmtp -t" || {  echo "OK " ; mail_setting_found=yes   ; } ;
 done
 
 
