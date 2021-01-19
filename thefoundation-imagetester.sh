6ceb9f3d0ece8d64fb29a5c1ffa7d59bcad2aff8
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-19 14:07:58 +0100

    2021-01-19-13.07

--- thefoundation-imagetester.sh
+++ thefoundation-imagetester.sh
@@ -190,7 +190,7 @@ for configtype in apache2 cli fpm;do
     grep "^sendmail_path" ${mailini} |grep -q "/usr/bin/msmtp -t" && {  echo "OK "  ; } ;
 done
 
-
+###### SQL TEST
 
 
 test_sql=no
@@ -203,8 +203,8 @@ echo "Y3JlYXRlIHRhYmxlIHRibEVtcGxveWVlCigKRW1wbG95ZWVfaWQgaW50IGF1dG9faW5jcmVtZW
 
 echo "##########"
 echo "SQL: mariadb OR mysql detected"|blue
+cat /tmp/sqlstatement.sql | mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create database $MYSQL_DATABASE ; use $MYSQL_DATABASE ;" 2>&1 |grep "ImageTestDB" |grep "database exists"
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create database $MYSQL_DATABASE ;use $MYSQL_DATABASE ;"
-cat /tmp/sqlstatement.sql | mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create database $MYSQL_DATABASE ; use $MYSQL_DATABASE ;"
 
 su -s /bin/bash -c 'mysql -e "show databases;use ${MYSQL_DATABASE} ; select * from tblEmployee where 1 = 1 ;"' www-data |grep -q Upadhya  || { build_ok=no ;fail_reasons=${fail_reasons}" mysql_no_user_in_mysql_mysql" ; }  ;
 mysql -e "use mysql;select * from user " |grep -q  user  || { build_ok=no ;fail_reasons=${fail_reasons}" mysql_no_user_in_mysql_user" ;  }  ;
