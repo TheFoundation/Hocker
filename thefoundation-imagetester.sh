59b6707472df63352cf4e88227bc82e748ad7507
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-19 19:47:55 +0100

    2021-01-19-18.47

--- thefoundation-imagetester.sh
+++ thefoundation-imagetester.sh
@@ -234,7 +234,7 @@ while ! test -f "/tmp/crontest.file" ; do
     [[ $(($(date -u +%s)-${start})) -gt 120 ]] && { echo;echo CRON::"TIMEOUT $(($(date -u +%s)-${start}))" ;  echo TIMEOUT >  /tmp/crontest.file ; } ;
     echo -ne $(
       echo -n "waiting since "$(($(date -u +%s)-${start}))" seconds | cron:"| blue |tr -d '\r\n';
-      ps aux|grep cron |grep -v grep|red |tr -d '\r\n';
+      ps aux|grep cron |grep -v grep head -c 50|red |tr -d '\r\n';
       echo -e " |logs: ";tail -c 70 /dev/shm/startlog |green |tr -d '\r\n'
     )'\r'  ;sleep 1;sleep 0.5; ## openwrt might not sleep 0.x
 done
