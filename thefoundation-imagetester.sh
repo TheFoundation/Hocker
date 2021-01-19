6e77dfcccd8fdab8570cddecb184cefeae03dbde
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-19 21:22:24 +0100

    2021-01-19-20.22

--- thefoundation-imagetester.sh
+++ thefoundation-imagetester.sh
@@ -212,7 +212,8 @@ echo "Y3JlYXRlIHRhYmxlIHRibEVtcGxveWVlCigKRW1wbG95ZWVfaWQgaW50IGF1dG9faW5jcmVtZW
 echo "##########"
 echo "SQL: mariadb OR mysql detected"|blue
 ### imort our test file
-cat /tmp/sqlstatement.sql | mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create database $MYSQL_DATABASE ; use $MYSQL_DATABASE ;" 2>&1
+echo                      | mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create database $MYSQL_DATABASE ; use $MYSQL_DATABASE ;" &>/dev/null
+cat /tmp/sqlstatement.sql | mysql -u root -p${MYSQL_ROOT_PASSWORD} $MYSQL_DATABASE  2>&1
 ### try to blindly create it again should say "database exists"
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create database $MYSQL_DATABASE ;use $MYSQL_DATABASE ;" 2>&1  |grep "ImageTestDB" |grep "database exists"  || { build_ok=no ;fail_reasons=${fail_reasons}" mysql_database_not_existing_after_import" ; }  ;
 
