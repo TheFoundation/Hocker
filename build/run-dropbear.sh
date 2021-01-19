60c18c7ac979d250fc7338fb9f41bbf752107f7b
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-19 16:35:40 +0100

    2021-01-19-15.35

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -351,10 +351,10 @@ mkfifo /dev/shm/supervisor_stdout_pipe
 
 echo " sys.info  | spawning supervisor"
 ##failed as well
-while (true);do cat /dev/shm/supervisor_stderr_pipe | sed 's/^[[:digit:]]\{4\}-[[:digit:]]\{2\}-[[:digit:]]\{2\} [[:digit:]]\{2\}:[[:digit:]]\{2\}:[[:digit:]]\{2\},[[:digit:]]\{3\} [[:upper:]]/ sys.info  | \0/g'  >/dev/stdout;sleep 0.2;done &
-while (true);do cat /dev/shm/supervisor_stdout_pipe | sed 's/^[[:digit:]]\{4\}-[[:digit:]]\{2\}-[[:digit:]]\{2\} [[:digit:]]\{2\}:[[:digit:]]\{2\}:[[:digit:]]\{2\},[[:digit:]]\{3\} [[:upper:]]/ sys.err   | \0/g'  > /dev/stderr;sleep 0.2;done &
-    exec $(which supervisord || echo /usr/bin/supervisord) -c /etc/supervisor/supervisord.conf   2>/dev/shm/supervisor_stderr_pipe 1>/dev/shm/supervisor_stdout_pipe
+#while (true);do cat /dev/shm/supervisor_stderr_pipe | sed 's/^[[:digit:]]\{4\}-[[:digit:]]\{2\}-[[:digit:]]\{2\} [[:digit:]]\{2\}:[[:digit:]]\{2\}:[[:digit:]]\{2\},[[:digit:]]\{3\} [[:upper:]]/ sys.info  | \0/g'  >/dev/stdout;sleep 0.2;done &
+#while (true);do cat /dev/shm/supervisor_stdout_pipe | sed 's/^[[:digit:]]\{4\}-[[:digit:]]\{2\}-[[:digit:]]\{2\} [[:digit:]]\{2\}:[[:digit:]]\{2\}:[[:digit:]]\{2\},[[:digit:]]\{3\} [[:upper:]]/ sys.err   | \0/g'  > /dev/stderr;sleep 0.2;done &
+#    exec $(which supervisord || echo /usr/bin/supervisord) -c /etc/supervisor/supervisord.conf   2>/dev/shm/supervisor_stderr_pipe 1>/dev/shm/supervisor_stdout_pipe
 
-
+exec $(which supervisord || echo /usr/bin/supervisord) -c /etc/supervisor/supervisord.conf 
 
 fi
