be2b51298940172b89bd7b45639db63093674c35
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-20 06:15:29 +0100

    2021-01-20-05.15

--- build/_1_php-initfpm.sh
+++ build/_1_php-initfpm.sh
@@ -8,10 +8,11 @@ find /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/ -name "*.
     test -e /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/conf.d/$(basename $file) || cp $file /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/conf.d/$(basename $file) ;
 done &
 
+sed 's/^error_log/.\+/error_log =\/dev\/stderr/g'  /etc/php/${PHPVersion}/fpm/pool.d/www.conf /etc/php/${PHPVersion}/fpm/php-fpm.conf
 
 (
 ## check disabled funtions
-grep ^'php_admin_value\[disable_functions\]'  /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf  ||  {
+grep ^'php_admin_value\[disable_functions\]'  /etc/php/$PHPVersion/fpm/pool.d/www.conf  ||  {
 
   ## typo3 needs exec     sometimes _> /var/www/typo3_src/
   ## laravel needs pcntl_async_signals with redis/horizon
@@ -29,7 +30,7 @@ grep ^'php_admin_value\[disable_functions\]'  /etc/php/$(php --version|head -n1|
   #if entered directly...
   echo "$PHP_FORBIDDEN_FUNCTIONS" |grep -q  "," && FORBIDDEN_FUNCTIONS_SELECTED=${PHP_FORBIDDEN_FUNCTIONS}
   [[ -z "$FORBIDDEN_FUNCTIONS_SELECTED" ]] &&  [[ "NONE" =  "${PHP_FORBIDDEN_FUNCTIONS}" ]] ||  echo "ERROR: set PHP_FORBIDDEN_FUNCTIONS to 'NONE' if you want to dangerously enable everything , it is currently: "${PHP_FORBIDDEN_FUNCTIONS}
-  [[ -z "$PHP_FORBIDDEN_FUNCTIONS" ]] && FORBIDDEN_FUNCTIONS_SELECTED=${FORBIDDEN_FUNCTIONS_DEFAULTS}  
+  [[ -z "$PHP_FORBIDDEN_FUNCTIONS" ]] && FORBIDDEN_FUNCTIONS_SELECTED=${FORBIDDEN_FUNCTIONS_DEFAULTS}
 
 
   fpmfile=/etc/php/${PHPVersion}/fpm/pool.d/www.conf
