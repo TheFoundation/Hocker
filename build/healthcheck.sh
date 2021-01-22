da973b8ff8c9e9a5693c916001b54961333d0850
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-22 16:10:02 +0100

    2021-01-22-15.10

--- build/healthcheck.sh
+++ build/healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 health_ok=yes
 supervisorctl status |grep -v "RUNNING"|wc -l |grep -v 0 || { health_ok=no ; supervisorctl status |grep -v "RUNNING" |sed 's/^/ERR-SUPERV-/g'; } ;
-/usr/bin/curl --fail -H "User-Agent: docker-health-check/over9000" -kL https://127.0.0.1/  > /dev/null ||  { health_ok=no ; echo FAIL80; } ;
-/usr/bin/curl --fail -H "User-Agent: docker-health-check/over9000" -kL https://127.0.0.1/  > /dev/null ||  { health_ok=no ; echo FAIL443; } ;
+/usr/bin/curl --fail -H "User-Agent: docker-health-check/over9000" -kL https://127.0.0.1/  > /dev/null ||  { health_ok=no ; echo FAIL80 ; } ;
+/usr/bin/curl --fail -H "User-Agent: docker-health-check/over9000" -kL https://127.0.0.1/  > /dev/null ||  { health_ok=no ; echo FAIL443 ; } ;
