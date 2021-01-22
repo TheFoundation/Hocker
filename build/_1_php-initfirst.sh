9dd1103156142376d6633cc9aafa5b5478f470aa
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-22 17:15:15 +0100

    2021-01-22-16.15

--- build/_1_php-initfirst.sh
+++ build/_1_php-initfirst.sh
@@ -80,29 +80,6 @@ which apache2ctl && (
     ) &
 
 
-(
-## fpm and apache fastcgi dislike php_value and php_admin_value in apache config
-sed 's/php_admin_value/#php_admin_value/g;s/php_value/#php_value/g' -i  /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/default-ssl.conf
-#grep "^docroot"                           /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf  ||  { (echo;echo "docroot = /var/www/html") >> /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf
-grep "^php_admin_value\[open_basedir\] = "  /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf  ||  { (echo;echo "php_admin_value[open_basedir] = /var/www/:/tmp/") >> /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf ; }
-grep "^php_value\[session.save_path\] = /var/www/.phpsessions"  /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf  ||  { (echo;echo "php_value[session.save_path] = /var/www/.phpsessions") >> /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf ; } ;
-
-find /etc/php/*/fpm/ -name www.conf |while read fpmpool;do
-  grep "^php_admin_flag\\[log_errors\\] = on" ${fpmpool} -q || echo "php_admin_flag[log_errors] = on" |tee -a ${fpmpool};
-                                                  done
-# FORCE php_admin_value[error_log] = /dev/stderr
-
-find /etc/php/*/fpm/ -name www.conf |while read fpmpool;do
-    grep '^php_admin_value\[error_log\] = /dev/stderr' ${fpmpool} |tail -n1 |grep 'php_admin_value\[error_log\] = /dev/stderr' ${fpmpool} || { echo -n " init.php |  $fpmpool" echo 'php_admin_value\[error_log\] = /dev/stderr' | tee -a ${fpmpool} ; } ;
-                                                  done
-
-# may the app get data from extenal urls
-[ "${DISALLOW_FOPEN}" = "true" ] && {
-  grep  ^'php_admin_value.allow_url_fopen.'  /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf |wc -l|grep -q 0$ || {
-      (echo;echo "php_admin_value[allow_url_fopen] = 0") >> /etc/php/$(php --version|head -n1|cut -d" " -f2|cut -d\. -f 1,2)/fpm/pool.d/www.conf ; } ;
-
-echo -n ; } ;
-) &
 
 (
 [  -z "${MAX_UPLOAD_MB}" ]  && MAX_UPLOAD_MB=128
