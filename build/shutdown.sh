a85f2e1c12648f177ad1545aafe02a5fb064f3b2
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-24 02:44:02 +0100

    2021-01-24-01.44

--- build/shutdown.sh
+++ build/shutdown.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-echo Sorry .. hanging up;sleep 0.5 & supervisorctl stop apache &
-mysql -e shutdown & supervisorctl stop php-fpm & sync &
-sleep 5
+echo Sorry .. hanging up;sleep 0.5 &
+
+echo stopping webservers ;  supervisorctl stop apache &>/dev/null &  supervisorctl stop nginx &
+sleep 0.5 # so fpm might drain last requests
+echo "stopping fpm ";       supervisorctl stop php-fpm &
+echo "stopping db"  ;       mysql -e shutdown &
+
+sleep 5 ## this could be quiet longer .. but docker :(
+
 supervisorctl stop mariadb 2>/dev/null
 supervisorctl stop mysql 2>/dev/null
 
+sync &
 sleep 3
 
 [[ -z $(supervisorctl pid|grep ^[0-9]) ]] || kill -SIGTERM $(supervisorctl pid|grep ^[0-9]) ;
