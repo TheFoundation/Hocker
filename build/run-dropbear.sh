ac21a59d541ff332e6ea6a4777b9c9496543ee0e
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-26 11:14:31 +0100

    2021-01-26-10.14

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -294,7 +294,7 @@ service_loop() {
                 test -e /dev/shm.cron.setup.${artisanfile//\//_} || {
                     echo " sys.cron  | artisan:schedule:loop -> ADDING: $CRONCMD" | lightblue >&2
                     (crontab -l -u www-data 2>/dev/null; echo "${CRONCMD}") | crontab -u www-data - ;
-                    which supervisorctl 2>&1 | grep -q supervisorctl && supervisorctl restart cron |tr -d '\n' &
+                    echo -n "restarting cron:";which supervisorctl 2>&1 | grep -q supervisorctl && supervisorctl restart cron |tr -d '\n' &
                     touch /dev/shm.cron.setup.${artisanfile//\//_}
                 echo ; } ;
             ##
