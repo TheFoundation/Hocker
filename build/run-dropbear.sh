52b91872299a4f4328bcd87afad3cf4669ac0218
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-26 12:56:27 +0100

    2021-01-26-11.56

--- build/run-dropbear.sh
+++ build/run-dropbear.sh
@@ -80,7 +80,7 @@ _supervisor_generate_artisanqueue() { ###supervisor queue:work
                          cat > /etc/supervisor/conf.d/queue_${artisanfile//\//_}.conf << EOF
 [program:laravel-worker]
 process_name=%(program_name)s_%(process_num)02d
-command=/usr/bin/php ${artisanfile} queue:work --timeout=14400 -sleep=3 --tries=3 --no-interaction --memory=2048 --daemon
+command=/usr/bin/php ${artisanfile} queue:work --timeout=14400 --sleep=3 --tries=3 --no-interaction --memory=2048 
 autostart=true
 autorestart=true
 user=www-data
