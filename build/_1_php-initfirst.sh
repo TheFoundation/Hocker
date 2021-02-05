e48a3a89665fe82dc94e8c295069ce35e19b6149
M°ŋα Ł¹ſα <commit@only.lan>
2021-02-05 02:15:57 +0100

    2021-02-05-01.15

--- build/_1_php-initfirst.sh
+++ build/_1_php-initfirst.sh
@@ -126,7 +126,7 @@ fi |while read myline;do echo  "${php_ini} : ${myline}";done
 
 find /etc/php/*/ -name php.ini |while read php_ini ;do
               sed 's/include_path.\+//g' ${php_ini} -i
-              echo "include_path = ./:/var/www/include_local:/var/www/include" | tee -a "${php_ini}" |while read myline;do echo  "${php_ini} : ${myline}";done
+              echo 'include_path = ./:/var/www/include_local:/var/www/include' | tee -a "${php_ini}" |while read myline;do echo  "${php_ini} : ${myline}";done
 done
 
 ) &
