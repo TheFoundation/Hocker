6a69daa240858110d01b08c50f12554e395d351c
M°ŋα Ł¹ſα <commit@only.lan>
2021-02-05 03:13:22 +0100

    2021-02-05-02.13

--- build/_1_php-initfirst.sh
+++ build/_1_php-initfirst.sh
@@ -118,6 +118,22 @@ done
 
 
 
+[[ -z "${PHP_SHORT_OPEN_TAG}" ]] || PHP_SHORT_OPEN_TAG="false"
+########if  [ "${PHP_SHORT_OPEN_TAG}" = "true" ]; then
+########  echo " init.php  | SETTING PHP_SHORT_OPEN_TAG:ON"
+########  find /etc/php/*/ -name php.ini |while read php_ini ;do
+########    sed 's/^short_open_tag.\+//g' ${php_ini} -i
+########     echo "short_open_tag = on"  | tee -a "${php_ini}"
+########  done  | sed 's/$/|/g' |tr -d '\n'
+########else
+########  echo -n " init.php  | SETTING PHP_SHORT_OPEN_TAG:OFF"
+########  find /etc/php/*/ -name php.ini |while read php_ini ;do  
+########                                 grep '^short_open_tag = off' "${php_ini}"  ||  {   echo -n " + ${php_ini} : + " ;
+########                                                                                 ( echo;echo "short_open_tag = off " ) >>  "${php_ini}"
+########                                                                                   grep  rt_open_tag ${php_ini} |grep -v '^;' ; } ;done
+########fi
+
+
 
 [[ -z "${PHP_ERROR_LEVEL}" ]] || PHP_ERROR_LEVEL="default"
 if [ "${PHP_ERROR_LEVEL}" = "default" ]; then
