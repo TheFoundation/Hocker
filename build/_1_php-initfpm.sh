68b4507ddf4ff118a3a70d9db1f268da42a126f5
M°ŋα Ł¹ſα <commit@only.lan>
2021-02-08 02:05:13 +0100

    2021-02-08-01.05

--- build/_1_php-initfpm.sh
+++ build/_1_php-initfpm.sh
@@ -116,7 +116,11 @@ echo -n ; } ;
           [[ -z "${PHP_SESSION_VALIDTIME_SECONDS}"  ]] && PHP_SESSION_VALIDTIME_SECONDS=68400
           [[ -z "${PHP_SESSION_CACHETIME_MINUTES}"  ]] && PHP_SESSION_CACHETIME_MINUTES=240
           for phpconf in $(find $(find /etc/ -maxdepth 1 -name "php*") -name php.ini |grep -e apache -e fpm);do
-              for valtoclear in session.gc_maxlifetime session.cookie_lifetime session.cache_expire   ;do sed "s/^${valtoclear}.\+//g" "${php_ini}" -i  ;done
+              # remove entries
+              for valtoclear in session.gc_maxlifetime session.cookie_lifetime session.cache_expire   ;do
+                  sed "s/^${valtoclear}.\+//g" "${php_ini}" -i ;
+              done
+              # add session settings
               ( echo  '[Session]';
                   echo "session.gc_maxlifetime  = ${PHP_SESSION_VALIDTIME_SECONDS}";
                   echo "session.cookie_lifetime = ${PHP_SESSION_VALIDTIME_SECONDS}";
